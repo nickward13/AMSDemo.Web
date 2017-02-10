7ef251b60077df518a4d8593de2bc5335a5218d5
Nick Ward <nickward@microsoft.com>
2017-02-10 16:21:03 +1100

    updated to TestShow2

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ var myOptions = {
 	width: "640",
 	height: "400",
 }
-
 myPlayer = amp("azuremediaplayer", myOptions);
-
 myPlayer.src([
         {
-                "src": "http://hectagonmedia.streaming.mediaservices.windows.net/071b7ec6-d0c7-4871-9543-bed233e7960d/1c4f8c7e-fefe-4c07-8dbb-9b7532137c1f.ism/manifest",
+                "src": "http://hectagonmedia.streaming.mediaservices.windows.net/c91799fb-43fa-4ac5-970b-e02f67dec40f/e4ad70fa-84f5-4ff3-b362-aece6594075e.ism/manifest",
                 "type": "application/vnd.ms-sstr+xml"
         }
 ]);
